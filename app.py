491c9a843c84dd6c1d4c10c1091f2b954d4c5a70
Defcon27 <defcon.sentinal95@gmail.com>
2020-12-30 18:21:31 +0530

    added image upload

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, redirect
 
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+
+
+
+#app.config["IMAGE_UPLOAD"] = ../../......
+
+
+@app.route('/upload-image', methods=['GET', 'POST'])
+def upload_image():
+
+    if request.method == "POST":
+        if request.files:
+            image = request.files["image"]
+            ext = image.filename.split(".")[-1]
+            if ext in ['.png', 'jpg', 'jpeg', 'bmp']:
+                user_imgname = "user_img."+ ext
+                image.save( os.path.join("static/images/", user_imgname) )
+                print("Image" + user_imgname + "Saved")
+            else:
+                print("Image Type not supported")
+
+            return redirect(request.url) 
+
+
+    return render_template("upload_image.html")
 
 
 
